21f3eab691be6baed6fdae0c7c13452f733d53fd
Radin Shayanfar <radin.shayanfar@gmail.com>
2020-07-31 19:42:24 +0430

    CRUD completed.

--- PictoController.py
+++ PictoController.py
@@ -9,18 +9,26 @@ class PictoController:
 
         tables = self._model.get_tables()
         self._view.sideListWidget.addItems(tables)
-        self._view.sideListWidget.currentItemChanged.connect(self.show_new_table)
+        self._view.sideListWidget.currentItemChanged.connect(self.new_table_clicked)
 
-    def show_new_table(self, new, prev):
-        columns, rows = self._model.get_table(new.text())
+    def new_table_clicked(self, new, prev):
+        self._load_table(new.text())
+
+    def _load_table(self, table_name=None):
+        columns, rows = self._model.get_table(table_name)
 
         self._view.tableGroupBox.buildTable(columns, rows, self.tableClick)
 
-    def tableClick(self, action: str, row_index: int):
-        row = [ln.text() for ln in self._view.tableGroupBox.lineEdits[row_index]]
+    def tableClick(self, action: str, row_index: int = 0):
+        if action == "insert":
+            row = [ln.text() for ln in self._view.tableGroupBox.insertLineEdits]
+            self._model.insert_row(row)
+            self._load_table()
+            return
+
+        row = [ln.text() for ln in self._view.tableGroupBox.tableLineEdits[row_index]]
         if action == "update":
             self._model.update_row(row, row_index)
         else:
-            # self._model.update_row(row)
-            pass
-        t = type(5)
+            self._model.delete_row(row_index)
+            self._load_table()
--- PictoModel.py
+++ PictoModel.py
@@ -3,6 +3,7 @@ class PictoModel:
         self._db = db
         self._cursor = self._db.cursor()
         self._dict_cursor = self._db.cursor(dictionary=True)
+        self._current_table_name = None
 
     def get_tables(self):
         self._cursor.execute("SHOW TABLES")
@@ -12,9 +13,10 @@ class PictoModel:
 
     def get_table(self, table_name: str):
         self._reset_lists()
-        self._current_table_name = table_name
+        if table_name is not None:
+            self._current_table_name = table_name
 
-        query = f"SHOW COLUMNS FROM {table_name}"
+        query = f"SHOW COLUMNS FROM {self._current_table_name}"
         self._dict_cursor.execute(query)
         for row in self._dict_cursor:
             self._current_columns.append(row['Field'])
@@ -22,7 +24,7 @@ class PictoModel:
             if row['Key'] == 'PRI':
                 self._current_table_keys.append(row['Field'])
 
-        query = f"SELECT * FROM {table_name}"
+        query = f"SELECT * FROM {self._current_table_name}"
         self._cursor.execute(query)
         rows = self._cursor.fetchall()
         for row in rows:
@@ -32,6 +34,7 @@ class PictoModel:
                     old.append(row[j])
             self._current_table_keys_old_vals.append(old)
 
+        self._db.commit()
         return self._current_columns, rows
 
     def update_row(self, row, row_index):
@@ -51,13 +54,36 @@ class PictoModel:
         self._db.commit()
         print(self._cursor.statement)
 
+    def delete_row(self, row_index):
+        query = f"DELETE FROM {self._current_table_name} WHERE "
+        for i, old_val in enumerate(self._current_table_keys_old_vals[row_index]):
+            query += f"{self._current_table_keys[i]}={old_val} AND "
+        query = query[:-5]
+
+        self._cursor.execute(query)
+        self._db.commit()
+        print(self._cursor.statement)
+
+    def insert_row(self, row):
+        self._convert_type(row)
+
+        query = f"INSERT INTO {self._current_table_name} VALUES ("
+        for i in range(len(row)):
+            query += "%s, "
+        query = query[:-2] + ")"
+
+        self._cursor.execute(query, row)
+        self._db.commit()
+        print(self._cursor.statement)
+
     def _convert_type(self, row):
         for i in range(len(self._current_table_types)):
             if self._current_table_types[i].startswith('int'):
-                row[i] = int(row[i])
+                row[i] = None if row[i] == '' else int(row[i])
+            else:
+                row[i] = str(row[i]) or None
 
     def _reset_lists(self):
-        self._current_table_name = None
         self._current_columns = []
         self._current_table_types = []
         self._current_table_keys = []
